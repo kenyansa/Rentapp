7d6a47f94bede7b6fa7650251da88176a3488fac
kenyansa <kenyansajustine@gmail.com>
2022-05-25 14:40:47 +0300

    polish foote section with bg color and boldening the text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,8 @@
 import {Box} from "@chakra-ui/react";
 
 const Footer = () =>(
-    <Box textAlign="center" p="5" color="#e8dddc" borderTop="1px" borderColor="blackAlpha.100">
-        Designed by KenTech Products Inc.{Kenyansa}, © 2022
+    <Box textAlign="center" p="5" bg="#e8beba" fontWeight="bold" borderTop="1px" borderColor="blackAlpha.100">
+        Designed by KenTech Products Inc.  © 2022 <br /> -----Kenyansa-----
     </Box>
 )
 export default Footer;
