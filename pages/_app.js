102fa9cf14a66ab583ad37e7d69331087a8c60c5
kenyansa <kenyansajustine@gmail.com>
2022-05-25 11:07:40 +0300

    create layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,21 @@ import Router from "next/router";
 import {ChakraProvider} from "@chakra-ui/react";
 import Head from "next/head";
 import NProgress from "nprogress";
-import Layout from "..components/Layout";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Head>
+
+  </Head>
+  <ChakraProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </ChakraProvider>
+  </>
+  )
 }
 
 export default MyApp
