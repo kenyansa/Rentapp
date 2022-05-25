e2179d66dde04df41e3fa513ae5e6b8039df624c
kenyansa <kenyansajustine@gmail.com>
2022-05-25 14:29:19 +0300

    create footer component with copyright details

---
+++ components/Footer.jsx
@@ -0,0 +1,8 @@
+import {Box} from "@chakra-ui/react";
+
+const Footer = () =>(
+    <Box textAlign="center" p="5" color="#e8dddc" borderTop="1px" borderColor="blackAlpha.100">
+        Designed by KenTech Products Inc.{Kenyansa}, © 2022
+    </Box>
+)
+export default Footer;
