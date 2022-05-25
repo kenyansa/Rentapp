bb89d86055aa33b1ee50cf58d42fb4ece4692817
kenyansa <kenyansajustine@gmail.com>
2022-05-25 10:19:26 +0300

    add price and property to tell if the property is verified

--- components/Property.jsx
+++ components/Property.jsx
@@ -14,6 +14,14 @@ const Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, ba
             <Box>
                 <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width="400" height="250" alt="mansionete" />
             </Box>
+            <Box w="full">
+                <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                    <Flex alignItems="center">
+                        <Box paddingRight="3" color="violet.400">{isVerified && <GoVerified />}</Box>
+                        <Text fontWeight="bold" fontSize="lg">KES{price}{rentFrequency && `/${rentFrequency}`}</Text>
+                    </Flex>
+                </Flex>
+            </Box>
         </Flex>
     </Link>
 )
