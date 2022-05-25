816b0b6e7553af3535449b1d3e22884293845d37
kenyansa <kenyansajustine@gmail.com>
2022-05-25 10:54:52 +0300

    create Layout component to display the header, i.e., navbar, body, and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+import Router from "next/router";
+import {ChakraProvider} from "@chakra-ui/react";
+import Head from "next/head";
+import NProgress from "nprogress";
+import Layout from "..components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
