d9a863bdb039e32f52dd8c17c9c85786ea7d99cc
kenyansa <kenyansajustine@gmail.com>
2022-05-27 00:12:42 +0300

    displaying properties of selected image of the rental property

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -7,11 +7,58 @@ import { baseUrl, fetchApi } from "../../utilities/fetchApi";
 import ImageScrollbar from "../../components/ImageScrollbar";
 
 //destructing the properties we need
-const PropertyDetails = ({propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos }}) => (
-    <Box maxWidth="1000px" margin="auto" p="4">
-       {photos && <ImageScrollbar data={photos} />} 
+const PropertyDetails = ({
+  propertyDetails: {
+    price,
+    rentFrequency,
+    rooms,
+    title,
+    baths,
+    area,
+    agency,
+    isVerified,
+    description,
+    type,
+    purpose,
+    furnishingStatus,
+    amenities,
+    photos,
+  },
+}) => (
+  <Box maxWidth="1000px" margin="auto" p="4">
+    {photos && <ImageScrollbar data={photos} />}
+    <Box w="full" p="6">
+      <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center">
+          <Box paddingRight="3" color="violet.400">
+            {isVerified && <GoVerified />}
+          </Box>
+          <Text fontWeight="bold" fontSize="sm">
+            KES{millify(price)}
+            {rentFrequency && `/${rentFrequency}`}
+          </Text>
+          <Box>
+            <Avatar size="sm" src={agency?.url} />
+          </Box>
+        </Flex>
+        <Flex
+          alignItems="center"
+          p="3"
+          justifyContent="space-between"
+          w="250px"
+          color="violet.400"
+        >
+          {rooms} <FaBed />| {baths}
+          <FaBath /> | {millify(area)} sqft <BsGridFill />
+        </Flex>
+        <Text fontSize="sm">
+          {/* displays the title of the photo is all characters are less than 50, else, it trancates the first 50 characters. */}
+          {title.length > 50 ? `${title.substring(0, 50)}...` : title}
+        </Text>
+      </Flex>
     </Box>
-)
+  </Box>
+);
 
 export default PropertyDetails;
 
