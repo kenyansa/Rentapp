f2585169a05d350e8e1888a4bcf20a5eae9574fe
kenyansa <kenyansajustine@gmail.com>
2022-05-26 22:23:35 +0300

    map over all the items and rendering options in each of the select elements

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,11 +3,40 @@ import {Flex, Select, Box, Text, Input, Spinner, Icon, Button} from "@chakra-ui/
 import {useRouter} from "next/router";
 import {MdCancel} from "react-icons/md"; //material design icons from react, for cancelling
 import Image from "next/image";
+import { filterData, getFilterValues } from "../utilities/filterData";
 
 const SearchFilters = () =>{
-    const [fitlers, setFilters] = useState({}); 
+    const [filters, setFilters] = useState(filterData);
+    const router = useRouter(); 
+
+    const searchProperties = (filterValues) =>{
+        const path = router.pathname;
+        const {query} = router;
+        const values = getFilterValues(filterValues);
+        values.forEach((item)=>{
+            query[item.name] = item.value
+        })
+        router.push({pathname:path, query})
+    }
     return(
         <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter)=>(
+                <Box key={filter.queryName}>
+                    <Select 
+                    placeholder={filter.placeholder}
+                    w="fit=content"
+                    p="3"
+                    onChange={(e)=> searchProperties({[filter.queryName]: e.target.value})}>
+                        {/* mapping over all the items and rendering options in each of the select elements. */}
+                        {filter?.items?.map((items)=>(
+                            <option value={items.value} key={items.value}
+                            >{items.name}
+                            </option>
+                        ))}
+                    </Select>
+                </Box>
+            )
+            )}
 
         </Flex>
     )
